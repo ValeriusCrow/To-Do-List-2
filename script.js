e9ca85147e9991a8f5c992bcdf545bb48789a325
ValeriusCrow <77670739+ValeriusCrow@users.noreply.github.com>
2021-06-12 13:42:28 +0300

    Implemented responsove design

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ taskList.forEach(element => {
     todoList.appendChild(newTask);
     let newCheckbox = document.createElement('input');
     newCheckbox.type = 'checkbox';
-    newTask.appendChild(newCheckbox);
+        newTask.appendChild(newCheckbox);
     let newSpan = document.createElement('span');
     newSpan.classList.add('task');
     newSpan.textContent = element['text'];
@@ -17,10 +17,13 @@ taskList.forEach(element => {
     let newButton = document.createElement('button');
     newButton.classList.add('delete-btn');
     newTask.appendChild(newButton);
-    let newIcon = document.createElement('i');
-    newIcon.classList.add('fas', 'fa-trash');
-    newButton.appendChild(newIcon);
-})
+    newButton.innerHTML = '<i class="fas fa-trash"></i>';
+    if (element.completed === true) {
+        newCheckbox.checked = true;
+        newTask.querySelector('span').classList.toggle('completed');
+    }
+    todoList.addEventListener('click', deleteCheck);
+});
 
 todoList.addEventListener('click', deleteCheck);
 
@@ -39,20 +42,17 @@ addButton.addEventListener('click', () => {
         let newButton = document.createElement('button');
         newButton.classList.add('delete-btn');
         newTask.appendChild(newButton);
-        let newIcon = document.createElement('i');
-        newIcon.classList.add('fas', 'fa-trash');
-        newButton.appendChild(newIcon);
+        newButton.innerHTML = '<i class="fas fa-trash"></i>';
         document.querySelector('#input-task').value = '';
         // Attaching listeners to new element
         todoList.addEventListener('click', deleteCheck);
         // Pushing new item to local storage
         const todo = {
-            id: Date.now(),
             text: newSpan.textContent,
             completed: false
         }
         taskList.push(todo);
-        console.log(newTask)
+        console.log(newTask);
         localStorage.setItem("tasks", JSON.stringify(taskList));
         console.log(taskList);
     }
@@ -61,13 +61,16 @@ addButton.addEventListener('click', () => {
 // Changing text-decoration of item or deleting it
 function deleteCheck(e) {
     const item = e.target;
+    const todoIndexText = item.parentElement.innerText;
+    const todoIndex = taskList.findIndex(elem => elem['text'] === todoIndexText);
     if (item.classList[0] === 'delete-btn') {
         item.parentElement.remove();
-
+        taskList.splice(todoIndex, 1);
     } else if (item.type === 'checkbox') {
-        item.parentElement.classList.toggle('completed');
         item.parentElement.querySelector('span').classList.toggle('completed');
+        taskList[todoIndex].completed = taskList[todoIndex].completed === false;
     }
+        localStorage.setItem("tasks", JSON.stringify(taskList));
 }
 
 
